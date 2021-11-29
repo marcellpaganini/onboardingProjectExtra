1c7a72337a4a1e82013c8b00fc2938600725b6b2
paganini.marcell <paganini.marcell@gmail.com>
2021-11-29 09:51:22 -0400

    removed customer/order fields

--- Types/Orders/Order.cs
+++ Types/Orders/Order.cs
@@ -8,10 +8,6 @@ namespace OrderManagement.Types.Orders
     public record Order
         {
             public Guid? Id { get; init; } = null;
-            public string CustomerName { get; init; } = "";
-            public string DeliveryAddress { get; init; } = "";
-            public string EmailAddress { get; init; } = "";
-            public string PhoneNumber { get; init; } = "";
             public DateTime OrderDate { get; init; } = new DateTime(01, 01, 0001);
             public int Status { get; init; } = 1;
             public ICollection<OrderItem> Items { get; init; } = new List<OrderItem>();
---
+++ WebClient/Migrations/20211129134753_RemovedCustomerOrderFields.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebClient.Migrations
+{
+    public partial class RemovedCustomerOrderFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerName",
+                table: "Order");
+
+            migrationBuilder.DropColumn(
+                name: "DeliveryAddress",
+                table: "Order");
+
+            migrationBuilder.DropColumn(
+                name: "EmailAddress",
+                table: "Order");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Order");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerName",
+                table: "Order",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeliveryAddress",
+                table: "Order",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmailAddress",
+                table: "Order",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Order",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
