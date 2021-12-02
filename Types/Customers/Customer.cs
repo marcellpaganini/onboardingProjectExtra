a47bd2f55dc6611dbde89bffc190a4f27360ed54
Marcell Tanure <paganini.marcell@gmail.com>
2021-12-02 16:30:51 -0400

    Added 1-* Emp-Cust model relationship

--- Types/Customers/Customer.cs
+++ Types/Customers/Customer.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
 using OrderManagement.Types.Orders;
+using OrderManagement.Types.Employees;
 
 namespace OrderManagement.Types.Customers
 {
     public record Customer
     {
         public Guid? Id { get; init; } = null;
+        public Guid EmployeeId { get; init; }
         public string FirstName { get; init; } = "";
         public string LastName { get; init; } = "";
         public string DeliveryAddress { get; init; } = "";
@@ -17,5 +19,6 @@ namespace OrderManagement.Types.Customers
         public string EmailAddress { get; init; } = "";
         public string PhoneNumber { get; init; } = "";
         public ICollection<Order> Orders { get; init; } = new List<Order>();
+        public Employee? Employee { get; init; } = null;
     }
 }
