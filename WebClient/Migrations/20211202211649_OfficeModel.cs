723fb9d4ba90f151a9886bf8cc587f323b67e706
Marcell Tanure <paganini.marcell@gmail.com>
2021-12-02 17:17:41 -0400

    OfficeModel Migration

---
+++ WebClient/Migrations/20211202211649_OfficeModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebClient.Migrations
+{
+    public partial class OfficeModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "OfficeId",
+                table: "Employee",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Office",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    City = table.Column<string>(type: "TEXT", nullable: false),
+                    Phone = table.Column<string>(type: "TEXT", nullable: false),
+                    Address = table.Column<string>(type: "TEXT", nullable: false),
+                    State = table.Column<string>(type: "TEXT", nullable: false),
+                    Country = table.Column<string>(type: "TEXT", nullable: false),
+                    PostalCode = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Office", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_OfficeId",
+                table: "Employee",
+                column: "OfficeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employee_Office_OfficeId",
+                table: "Employee",
+                column: "OfficeId",
+                principalTable: "Office",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employee_Office_OfficeId",
+                table: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "Office");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employee_OfficeId",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "OfficeId",
+                table: "Employee");
+        }
+    }
+}
