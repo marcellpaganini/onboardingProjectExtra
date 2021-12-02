201238a42d007c21233944f11eab36a9e2a2edc0
Marcell Tanure <paganini.marcell@gmail.com>
2021-12-02 16:36:10 -0400

    migration EmployeeModel

---
+++ WebClient/Migrations/20211202203211_EmployeeModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebClient.Migrations
+{
+    public partial class EmployeeModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "EmployeeId",
+                table: "Customer",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Manager = table.Column<string>(type: "TEXT", nullable: false),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    EmailAddress = table.Column<string>(type: "TEXT", nullable: false),
+                    Extension = table.Column<string>(type: "TEXT", nullable: false),
+                    JobTitle = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customer_EmployeeId",
+                table: "Customer",
+                column: "EmployeeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customer_Employee_EmployeeId",
+                table: "Customer",
+                column: "EmployeeId",
+                principalTable: "Employee",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customer_Employee_EmployeeId",
+                table: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customer_EmployeeId",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeId",
+                table: "Customer");
+        }
+    }
+}
